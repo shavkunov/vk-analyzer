9faace3e8f327244a0695397a8ac5e7a879073df
shavkunov <mv.shavkunov@yandex.ru>
2017-08-19 04:11:03 +0300

    added one more test

--- internal-core/src/test/java/ru/spbau/shavkunov/StatisticsTest.java
+++ internal-core/src/test/java/ru/spbau/shavkunov/StatisticsTest.java
@@ -1,9 +1,17 @@
 package ru.spbau.shavkunov;
 
 import org.junit.Test;
+import ru.spbau.shavkunov.primitives.PostQuantity;
 import ru.spbau.shavkunov.primitives.Statistics;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Mikhail Shavkunov
@@ -18,5 +26,35 @@ public class StatisticsTest {
         double value1 = 5/3.0;
         double actual1 = Statistics.getDoubleWithPrecision(value1, 2);
         assertEquals(1.67, actual1, 0);
+    }
+
+    @Test
+    public void countAverageQuantityTest() throws Exception {
+        Statistics stats = mock(Statistics.class);
+        Method method = Statistics.class.getDeclaredMethod("countAverageQuantity", PostQuantity.class, List.class);
+        method.setAccessible(true);
+
+        PostQuantity views = PostQuantity.VIEWS;
+        PostQuantity reposts = PostQuantity.REPOSTS;
+        PostQuantity likes = PostQuantity.LIKES;
+
+        List list = new ArrayList();
+        Map entity1 = mock(Map.class);
+        Map entity2 = mock(Map.class);
+        list.add(entity1);
+        list.add(entity2);
+
+        Map count1 = mock(Map.class);
+        when(count1.get("count")).thenReturn(5);
+        when(entity1.containsKey(likes)).thenReturn(true);
+        Map count2 = mock(Map.class);
+        when(count2.get("count")).thenReturn(3);
+        when(entity2.containsKey(likes)).thenReturn(true);
+
+        when(entity1.get(likes)).thenReturn(count1);
+        when(entity2.get(likes)).thenReturn(count2);
+
+        double actual = (double) method.invoke(stats, likes, list);
+        assertEquals(4.0, actual, 0);
     }
 }
